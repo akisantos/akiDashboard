d759cb5e8071a018c2db09689e1e69b84438bc8d
akkii0609 <38753836+akisantos@users.noreply.github.com>
2023-10-05 16:19:07 +0700

    onchange filter to chart

--- static/src/components/chart/chart.js
+++ static/src/components/chart/chart.js
@@ -1,39 +1,82 @@
 /** @odoo-module */
 import { registry } from "@web/core/registry"
 import {loadJS} from "@web/core/assets"
-const { Component, onWillStart, useRef, onMounted } = owl
+const { Component, onWillStart, useRef, onMounted, onRendered, useEffect } = owl
 
 export class ChartRenderer extends Component{
     setup(){
+
         this.chartRef = useRef('chart')
+        this.chart = ''
         onWillStart(async ()=>{
             await loadJS("https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.0/chart.umd.min.js")
 
         })
 
-        onMounted(()=>this.renderChart())
+        useEffect(() => {
+          if (this.chart ==''){
+            console.log("Not rendered");
+          }else{
+            this.removeData();
+            this.addData(this.props.data);
+          }
+
+
+        },()=>[this.props.data]);
+
+        onMounted(()=>
+          this.renderChart()
+        )
+    
     }
 
+    addData(newData) {
+      this.chart.data.datasets.forEach((dataset) => {
+          dataset.data = newData;
+      });
+      this.chart.update();
+    }
+
+    removeData() {
+        this.chart.data.datasets.forEach((dataset) => {
+            dataset.data = [];
+        });
+        this.chart.update();
+    }
+   
+
+    
+
     renderChart(){
-        new Chart(this.chartRef.el,
+        let nameLabels = this.props.dataLabels;
+        let chartData = this.props.data;
+        
+        this.chart = new Chart(this.chartRef.el,
         {
           type: this.props.type,
+          
           data: {
-            labels: this.props.dataLabels,
-              datasets: [this.props.data]
+            datasets: [{
+              label: 'Bán chạy nhất',
+              data: this.props.data,
+            }],
+            
           },
+
           options: {
-            responsive: true,
             plugins: {
-              legend: {
-                position: 'bottom',
-              },
-              title: {
-                display: true,
-                text: this.props.title,
-                position: 'bottom',
-              }
-            }
+                tooltip: {
+                    callbacks: {
+                        label: function(tooltipItem) {
+                          return 'Sản phẩm: ' + nameLabels[tooltipItem.dataIndex]
+                        },
+                        title: function(tooltipItem){
+                          return 'Doanh thu: ' + chartData.at(tooltipItem.dataIndex).y;
+                        }
+                    }
+                }
+            },
+          
           },
         }
       );
--- static/src/components/sale_dashboard.js
+++ static/src/components/sale_dashboard.js
@@ -8,44 +8,166 @@ const { Component, onWillStart, useRef, onMounted, useState } = owl
 
 export class AkiSaleDashboard extends Component{
     setup(){
-        // this.orm = useService("orm");
-        // onWillStart(async () => {
-        //        await this.loadDashboardData();
-        // });
+        this.orm = useService("orm");
+        this.actionService = useService("action")
+
+        this.state = useState({
+            quotations: {
+                value:10,
+                percentage:6,
+            },
+
+            period: 90,
+        })
+    
+        // this.dashboard_service = useService("akiDashboardServices")
         
 
-        this.dashboard_service = useService("akiDashboardServices")
-        console.log(this.dashboard_service)
-        this.dashboard_data = useState(this.dashboard_service.dashboard_data)
-        this.sale_data = useState(this.dashboard_service.sale_data)
+        onWillStart(async ()=>{
+            this.getDates();
+            await this.GetBestSellerChart();
+            await this.getQuotation();
+            await this.getOrders();
+
+        })
     }
 
-//    async loadDashboardData() {
-//       const context = {};
-//       this.purchase_order_count = await this.orm.call(
-//           'res.partner',
-//           'get_sale_order_count',
-//           [],
-//           {
-//               context: context
-//           }
-//       );
-//
-//       console.log(this.purchase_order_count)
-//   }
-    // async loadDashboardData() {
-    //        const context = {};
-    //        this.purchase_order_count = await this.orm.call(
-    //            'sale.order',
-    //            'get_purchase_order_count',
-    //            [],
-    //            {
-    //                context: context
-    //            }
-    //        );
-    //    }
+
+    async onPeriodFilterChange(){
+        this.getDates();
+        console.log(moment().subtract(this.state.period, 'days').calendar());
+        await this.getQuotation();
+        await this.getOrders();
+        await this.GetBestSellerChart();
+    }
+
+    getDates(){
+        this.state.currentDate = moment().subtract(this.state.period,'days').format('L');
+        this.state.previousDate = moment().subtract(this.state.period * 2,'days').format('L');
+
+    }
+
+    async getQuotation(){
+        let domain = [['state', 'in', ['sent', 'draft']]];
+        let prevDomain = domain;
+
+        if (this.state.period >0){
+            domain.push(['date_order','>=',this.state.currentDate])
+            prevDomain.push(['date_order','>=', this.state.previousDate], ['date_order','<=', this.state.currentDate]);
+        }
+
+        const data = await this.orm.searchCount("sale.order", domain);
+        this.state.quotations.value = data;
+
+        // Tính % kì trước  
+        const prevData = await this.orm.searchCount('sale.order', prevDomain);
+        const phanTram = (data - prevData)/ prevData *100
+        this.state.quotations.percentage = phanTram;
+
+    }
+    
+    async getOrders(){
+        let domain = [['state','in',['done', 'sale']]];
+        let prevDomain = [['state','in',['done', 'sale']]];
+
+        if (this.state.period > 0){
+            domain.push(['date_order','>=', this.state.currentDate])
+            prevDomain.push(['date_order','>=', this.state.previousDate], ['date_order','<=', this.state.currentDate]);
+        }
+        const data = await this.orm.searchCount('sale.order',domain);
+
+        // Tính % kì trước  
+        const prevData = await this.orm.searchCount('sale.order', prevDomain);
+        const phanTram = (data - prevData)/(prevData) * 100;
+
+        //Tính đơn thành công (đã xuất hoá đơn)
+        let invoicedDomain = domain;
+        invoicedDomain.push(['invoice_status', 'in' ,['invoiced', 'to invoice']])
+        const invoicedOrderCount = await this.orm.searchCount('sale.order', invoicedDomain);
+
+        //Tính revenue
+        let prevRevenueDomain = prevDomain;
+        prevRevenueDomain.push(['invoice_status', 'in',['invoiced', 'to invoice']])
+        
+        const revenue = await this.orm.readGroup('sale.order', domain, ['amount_total:sum'], []);
+        const prevRevenue = await this.orm.readGroup('sale.order', prevRevenueDomain, ['amount_total:sum'], []);
+        const revenuePercent = (revenue[0].amount_total - prevRevenue[0].amount_total)/ (prevRevenue[0].amount_total) * 100;
+
+        //Tính profit
+        let profit = await this.orm.readGroup('sale.order', domain, ['amount_untaxed:sum'], []);
+
+        //Tính average Revenue
+        let averageRevenue = revenue[0].amount_total / invoicedOrderCount;
+
+        //Trả kết quả
+        this.state.orders = {
+            count: data,
+            percentage: phanTram.toFixed(1),
+            invoicedOrders: invoicedOrderCount,
+            revenue: `${(revenue[0].amount_total)}₫`,
+            revenuePercent: revenuePercent.toFixed(1),
+            profit:`${(profit[0].amount_untaxed)}₫`,
+            averageRevenue: averageRevenue.toFixed(2)
+        }
+    }
+
+    async viewQuotation(){
+
+        // let domain = [['state', 'in', ['sent', 'draft']]]
+        // if (this.state.period > 0){
+        //     domain.push(['date_order','>', this.state.current_date])
+        // }
+
+        // let list_view = await this.orm.searchRead("ir.model.data", [['name', '=', 'view_quotation_tree_with_onboarding']], ['res_id'])
+
+        // this.actionService.doAction({
+        //     type: "ir.actions.act_window",
+        //     name: "Quotations",
+        //     res_model: "sale.order",
+        //     domain,
+        //     views: [
+        //         [list_view.length > 0 ? list_view[0].res_id : false, "list"],
+        //         [false, "form"],
+        //     ]
+        // })
+
+        this.none()
+
+    }
+
+    none(){
+        
+    }
+    async GetBestSellerChart(){
+        let domain = [['state', 'in', ['done', 'sale']]]
+        
+        domain.push(['invoice_status', 'in' ,['invoiced', 'to invoice']])
+        if (this.state.period >0){
+            domain.push(['date_order','>=',this.state.currentDate])
+        }
+
+        let chartData = await this.orm.searchRead('sale.order',domain, ['name','amount_total','date_order'])
+        let saleList = []
+        let saleListLabels = []
+        for(let i =0;i< chartData.length; i++){
+            let saleData ={
+                x: moment(new Date(chartData[i].date_order)).format("DD/MM/YYYY"),
+                y: chartData[i].amount_total,
+
+            }
+            saleList.push(saleData);
+            saleListLabels.push(chartData[i].name)
+        }
+
+
+        this.state.saleData = {
+            label: saleListLabels,
+            data: saleList,
+        }
+
+    }
 }
 
 AkiSaleDashboard.template = "aki_dashboard.AkiSaleDashboard"
 AkiSaleDashboard.components = { KpiCard, ChartRenderer}
-registry.category("actions").add("aki_dashboard.AkiSaleDashboard", AkiSaleDashboard);
+registry.category("actions").add("aki_dashboard.AkiSaleDashboard", AkiSaleDashboard)
